7f8ec4014a035da3fa36cf857880fcf835c3c524
ElismarSilva <elismar.timaosilva@gmail.com>
2023-03-21 20:10:33 -0300

    first commit

---
+++ sensor_de_umidade.ino
@@ -0,0 +1,27 @@
+int umidade;
+ 
+void setup()
+{
+ Serial.begin(9600);
+ Serial.println("www.usinainfo.com.br");
+ pinMode(13, OUTPUT);
+}
+void loop()
+{
+ umidade = analogRead(A0);
+ int Porcento = map(umidade, 1023, 0, 0, 100);
+ 
+ Serial.print(Porcento);
+ Serial.println("%");
+ if(Porcento <=40)
+ {
+ Serial.println("Irrigando...");
+ digitalWrite(13, HIGH);
+ }
+ 
+ else
+ {
+ digitalWrite(13, LOW);
+ }
+ delay(1000);
+}
